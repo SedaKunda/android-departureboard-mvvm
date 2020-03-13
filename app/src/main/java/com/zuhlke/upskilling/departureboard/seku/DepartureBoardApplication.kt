ea5e470304bf7894968d780a325297812f155acc
seku <seda.kunda@zuhlke.com>
2020-03-13 16:31:40 +0000

    Add dark theme support

--- app/src/main/java/com/zuhlke/upskilling/departureboard/seku/DepartureBoardApplication.kt
+++ app/src/main/java/com/zuhlke/upskilling/departureboard/seku/DepartureBoardApplication.kt
@@ -1,11 +1,13 @@
 package com.zuhlke.upskilling.departureboard.seku
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import timber.log.Timber
 
 class DepartureBoardApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode());
     }
 }
